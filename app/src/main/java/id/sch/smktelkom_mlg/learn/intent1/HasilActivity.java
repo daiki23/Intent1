9305f41a883ce8d2b3c156f918c813f8fb531ece
daiki23 <r.dikaadjI@gmail.com>
2017-01-24 02:48:27 +0700

    startActiity Part2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -1,20 +1,36 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.textservice.TextInfo;
+import android.widget.TextView;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class HasilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+
+        setTitle("Hasil");
         
 
         
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR,0);
+
+        int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunLahir  = yearNow - umur ;
+
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + "lahir pada tahun" + tahunLahir);
         
     }
 
@@ -29,4 +45,6 @@ public class HasilActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
